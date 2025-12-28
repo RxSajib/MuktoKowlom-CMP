6a111e0b85f7d340c9b13f03a2d058e71ab1c5e0
RxSajib <sajibroy206@gmail.com>
2025-12-28 14:38:35 +0600

    init project

--- composeApp/src/iosMain/kotlin/com/aliad/muktokowlom/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/aliad/muktokowlom/MainViewController.kt
@@ -3,5 +3,7 @@ package com.aliad.muktokowlom
 import androidx.compose.ui.window.ComposeUIViewController
 import com.aliad.muktokowlom.di.initKoin
 
-fun MainViewController() = ComposeUIViewController { initKoin()
-    App() }
+fun MainViewController() = ComposeUIViewController {
+    initKoin()
+    App()
+}
