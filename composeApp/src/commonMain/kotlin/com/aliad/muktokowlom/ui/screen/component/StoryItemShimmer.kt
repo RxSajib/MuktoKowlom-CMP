d73226f8766ef5e3cdc994805f323dac33d41658
RxSajib <sajibroy206@gmail.com>
2026-02-22 23:40:01 +0600

    feat: implement story item shimmer effect

---
+++ composeApp/src/commonMain/kotlin/com/aliad/muktokowlom/ui/screen/component/StoryItemShimmer.kt
@@ -0,0 +1,66 @@
+package com.aliad.muktokowlom.ui.screen.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+import com.aliad.muktokowlom.ui.theme.adjustedFontSize
+import com.valentinilk.shimmer.shimmer
+import muktokowlomcmp.composeapp.generated.resources.Res
+import muktokowlomcmp.composeapp.generated.resources.placeholder
+import org.jetbrains.compose.resources.painterResource
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@Composable
+fun StoryItemShimmer(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier.fillMaxWidth().shimmer().clip(shape = RoundedCornerShape(10.dp))
+            .padding(10.dp)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxWidth().height(100.dp)
+                .background(color = MaterialTheme.colorScheme.inverseSurface.copy(alpha = 0.2f))
+                .clip(shape = RoundedCornerShape(10.dp))
+        )
+        HeightGap(20.dp)
+        Box(
+            modifier = Modifier.fillMaxWidth(.5f).height(10.dp)
+                .background(color = MaterialTheme.colorScheme.inverseSurface.copy(alpha = 0.2f))
+        )
+        HeightGap(10.dp)
+
+        Box(
+            modifier = Modifier.fillMaxWidth().height(30.dp)
+                .background(color = MaterialTheme.colorScheme.inverseSurface.copy(alpha = 0.2f))
+        )
+    }
+}
+
+@Composable
+fun StoryShimmerRow(){
+    Row {
+        StoryItemShimmer(modifier = Modifier.weight(1f))
+        StoryItemShimmer(modifier = Modifier.weight(1f))
+    }
+}
+
+@Composable
+@Preview
+fun StoryItemShimmerPreview() {
+    StoryShimmerRow()
+}
