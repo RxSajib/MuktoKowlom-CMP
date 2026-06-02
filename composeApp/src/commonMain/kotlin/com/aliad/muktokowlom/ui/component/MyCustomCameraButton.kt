d4b8d296bde5a5611fd10f041d6706045595c3ce
RxSajib <sajibroy206@gmail.com>
2026-06-02 23:27:41 +0600

    feat: my custom camera button ui design

--- composeApp/src/commonMain/kotlin/com/aliad/muktokowlom/ui/component/MyCustomCameraButton.kt
+++ composeApp/src/commonMain/kotlin/com/aliad/muktokowlom/ui/component/MyCustomCameraButton.kt
@@ -14,6 +14,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.unit.dp
 import com.aliad.muktokowlom.ui.theme.onPrimaryLight
 import muktokowlomcmp.composeapp.generated.resources.Res
@@ -23,11 +24,12 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
 fun MyCustomCameraButton(){
-    Box(modifier = Modifier.size(25.dp).clip(shape = CircleShape).border(width = 2.dp, color = MaterialTheme.colorScheme.surface, shape = CircleShape).background(color = onPrimaryLight), contentAlignment = Alignment.Center){
+    Box(modifier = Modifier.size(25.dp).clip(shape = CircleShape).background(color = onPrimaryLight).border(width = 2.dp, color = MaterialTheme.colorScheme.surface, shape = CircleShape), contentAlignment = Alignment.Center){
         Image(
             painter = painterResource(Res.drawable.camera_svgrepo_com),
             contentDescription = null,
             modifier = Modifier.fillMaxWidth(.5f).aspectRatio(1f),
+            colorFilter = ColorFilter.tint(color = Color.White)
         )
     }
 }
